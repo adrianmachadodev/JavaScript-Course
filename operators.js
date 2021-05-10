6cb163f79888b3c985d2e878c88fcba78b7bce9f
Adrian Machado <machadoadrian9705@gmail.com>
2021-05-10 18:12:17 -0300

    Operators

---
+++ operators.js
@@ -0,0 +1,13 @@
+/*operadores matematicos */
+let a = 5;
+let b = 2;
+
+let usuario = "Pedro";
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+console.log ("hola" + usuario);
