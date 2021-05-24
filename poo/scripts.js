cd18de0e633d1468cae258f5471a7bb0554130b4
Adrian Machado <machadoadrian9705@gmail.com>
2021-05-24 11:50:40 -0300

    POO-clases

--- poo/scripts.js
+++ poo/scripts.js
@@ -1,4 +1,23 @@
 //Programacion orientada a objetos (POO)
+class Persona{
+    constructor(nombre, apellido, edad){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
 
-S
+        this.datos = `Me  llamo ${nombre} ${apellido} y tengo ${edad} años`
+    }
 
+
+    saludar(){
+        return `Hola me llamo ${this.nombre} y tengo ${this.edad} años.`
+
+    }
+  
+}   
+
+const juan = new Persona ('Juan', 'Garcia', '23');
+const marta = new Persona ('Marta', 'Garcia', '33');
+
+console.log(juan.saludar());
+console.log(marta.saludar());
