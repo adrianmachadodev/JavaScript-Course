9e126f6d27b39fdd13a858e7090e86a68a2b031b
Adrian Machado <machadoadrian9705@gmail.com>
2021-06-20 17:22:02 -0300

    API - Online - offline - drag and drop

---
+++ API Online y Offline/scripts.js
@@ -0,0 +1,27 @@
+const alert = document.getElementById('alert')
+
+addEventListener('online', (e) =>{
+    setAlert(1);
+})  
+
+addEventListener('offline', (e) =>{
+    setAlert(0);
+})
+
+const setAlert = (status) =>{
+
+   alert.classList.remove('aler--online')
+   alert.classList.remove('aler--offline')
+
+   status === 0? 
+        setTimeout(()=>{
+            alert.textContent = "Ups, it seems you 're offline"
+            alert.classList.add('alert--offline')
+        },100):
+        setTimeout(() => {
+            alert.textContent = "Great! You're online again!"
+            alert.classList.add('alert--online')
+        }, 100)
+    
+    
+}
---
+++ Drag & Drop + API File/scripts.js
@@ -0,0 +1,19 @@
+const fileInput = document.getElementById('file')
+const dropZone = document.getElementById('drop-zone')
+
+dropZone.addEventListener('click', () => fileInput.click())
+
+dropZone.addEventListener('dragover', (e) =>{
+    e.preventDefault()
+    dropZone.classList.add('drop-zone--active')
+})
+
+dropZone.addEventListener('dragleave', (e) =>{
+    e.preventDefault()
+    dropZone.classList.remove('drop-zone--active')
+})
+
+dropZone.addEventListener('drop', (e) =>{
+    e.preventDefault()
+    console.log(e.dataTransfer)
+})
